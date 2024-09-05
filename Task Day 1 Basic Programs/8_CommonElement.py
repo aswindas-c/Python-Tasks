9683b6253f03ffb4fe6fa440bf76e3f6bbbeeefa
Aswindas Chemban(UST <285151@ust.com>
2024-09-05 14:25:32 +0530

    Exception

--- Task Day 1 Basic Programs/8_CommonElement.py
+++ Task Day 1 Basic Programs/8_CommonElement.py
@@ -1,19 +1,21 @@
 list1 =[]
-n =  int(input("Enter the size of list 1: "))
-print("Enter the elements of list 1 : \n")
-for i in range(0,n):
-    num = int(input())
-    list1.append(num)
-list2 =[]
-list3 = []
-n =  int(input("Enter the size of list 2: "))
-print("Enter the elements of list 2 : \n")
-for i in range(0,n):
-    num = int(input())
-    list2.append(num)
-print("Common Elements")
-for i in list1:
-    if i in list2:
-        list3.append(i)
-print(list3)
-
+try:
+    n =  int(input("Enter the size of list 1: "))
+    print("Enter the elements of list 1 : \n")
+    for i in range(0,n):
+        num = input()
+        list1.append(num)
+    list2 =[]
+    list3 = []
+    n =  int(input("Enter the size of list 2: "))
+    print("Enter the elements of list 2 : \n")
+    for i in range(0,n):
+        num = input()
+        list2.append(num)
+    print("Common Elements")
+    for i in list1:
+        if i in list2:
+            list3.append(i)
+    print(list3)
+except ValueError:
+    print("Enter Integer Value")
