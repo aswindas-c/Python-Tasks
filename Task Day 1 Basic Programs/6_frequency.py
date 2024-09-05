833b01904965ebeba35e6209ba6662fe23500d91
Aswindas Chemban(UST <285151@ust.com>
2024-09-05 14:15:53 +0530

    Exception

--- Task Day 1 Basic Programs/6_frequency.py
+++ Task Day 1 Basic Programs/6_frequency.py
@@ -1,13 +1,16 @@
-list =[]
-n =  int(input("Enter the size of list : "))
-print("Enter the elements of list : \n")
-for i in range(0,n):
-    num = int(input())
-    list.append(num)
-frequency = {}
-for i in list:
-    if i in frequency:
-        frequency[i] +=1
-    else:
-        frequency[i] = 1
-print(frequency)
+number_list =[]
+try:
+    n =  int(input("Enter the size of list : "))
+    print("Enter the elements of list : \n")
+    for i in range(0,n):
+        num = input()
+        number_list.append(num)
+    frequency = {}
+    for i in number_list:
+        if i in frequency:
+            frequency[i] +=1
+        else:
+            frequency[i] = 1
+    print(frequency)
+except ValueError:
+    print("Enter Integer Value")
