7dd582eca0831759f0ca9d0128cbfea91e1e4335
Aswindas Chemban(UST <285151@ust.com>
2024-09-05 14:54:50 +0530

    Exception and case for negative number

--- Task Day 1 Basic Programs/10_SecondLargest.py
+++ Task Day 1 Basic Programs/10_SecondLargest.py
@@ -1,13 +1,24 @@
-list =[]
-n =  int(input("Enter the size of list : "))
-print("Enter the elements of list : \n")
-for i in range(0,n):
-    num = int(input())
-    list.append(num)
-large = 0
-second = 0
-for i in range(0,n):
-    if large<list[i]:
-        second = large
-        large=list[i]
-print(f"Second Largest = {second}")
+import sys
+
+number_list =[]
+try:
+    n =  int(input("Enter the size of list : "))
+    print("Enter the elements of list : \n")
+    for i in range(0,n):
+        num = int(input())
+        number_list.append(num)
+    large =number_list[0]
+    second = number_list[1]
+    if large < second:
+        temp = large
+        large = second
+        second = temp
+    for i in range(2,n):
+        if large<number_list[i]:
+            second = large
+            large=number_list[i]
+        elif second<number_list[i]:
+            second = number_list[i]
+    print(f"Second Largest = {second}")
+except ValueError:
+    print("Enter Integer Number")
