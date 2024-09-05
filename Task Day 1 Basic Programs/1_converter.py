42c43357bd93fd8475a34a1e0a3d992a4148f324
Aswindas Chemban(UST <285151@ust.com>
2024-09-05 12:21:17 +0530

    Exception

--- Task Day 1 Basic Programs/1_converter.py
+++ Task Day 1 Basic Programs/1_converter.py
@@ -1,19 +1,22 @@
 i=0
 while i < 5:
-    choice = int(input("Select from Below\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Inches to cm\n4.Cm to inch\n5.Exit\n"))
-    if choice==1:
-        f = float(input("Enter the temperature in Fahrenheit : \n"))
-        print(f"Celsius = {round(((f - 32.0) * 5.0 / 9.0),2)}")
-    elif choice==2:
-        c=float(input("Enter the temperature in Celsius : \n"))
-        print(f"Fahrenheit = {round(((c * 9/5) + 32),2)}")
-    elif choice == 3:
-        n = float(input(f"Enter the length in Inch : \n"))
-        print(f"Cm = {round((n*2.54),2)}")
-    elif choice==4:
-        n = float(input("Enter the length in Cm : \n"))
-        print(f"Inch = {round((n/2.54),2)}")
-    elif choice==5:
-        exit()
-    else:
-        print("Enter Valid Input")
+    try:
+        choice = int(input("Select from Below\n1.Fahrenheit to Celsius\n2.Celsius to Fahrenheit\n3.Inches to cm\n4.Cm to inch\n5.Exit\n"))
+        if choice==1:
+            f = float(input("Enter the temperature in Fahrenheit : \n"))
+            print(f"Celsius = {round(((f - 32.0) * 5.0 / 9.0),2)}")
+        elif choice==2:
+            c=float(input("Enter the temperature in Celsius : \n"))
+            print(f"Fahrenheit = {round(((c * 9/5) + 32),2)}")
+        elif choice == 3:
+            n = float(input(f"Enter the length in Inch : \n"))
+            print(f"Cm = {round((n*2.54),2)}")
+        elif choice==4:
+            n = float(input("Enter the length in Cm : \n"))
+            print(f"Inch = {round((n/2.54),2)}")
+        elif choice==5:
+            exit()
+        else:
+            print ("Invalid Input")
+    except ValueError as e:
+        print(f"Error : {e}")
