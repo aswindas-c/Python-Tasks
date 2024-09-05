ad27467491c5b909d9ecd9ba0e6a45a431bc674d
Aswindas Chemban(UST <285151@ust.com>
2024-09-05 12:34:46 +0530

    Exception

--- Task Day 1 Basic Programs/3_factorial.py
+++ Task Day 1 Basic Programs/3_factorial.py
@@ -2,5 +2,10 @@ def factorial(n):
     if n==1 or n==0:
         return 1
     return n*factorial(n-1)
-number = int(input("Enter the number : \n"))
-print(f"Factorial = {factorial(number)}")
+try:
+    number = int(input("Enter the number : \n"))
+    print(f"Factorial = {factorial(number)}")
+except ValueError:
+    print("Enter a Integer Number")
+except RecursionError:
+    print("Enter Positive Number")
