736d896fee9548c7a5f6aa611b8194b0c1a9df50
Aswindas Chemban(UST <285151@ust.com>
2024-09-05 15:27:07 +0530

    Exception

--- Task Day 1 Basic Programs/CountDownIbuilt.py
+++ Task Day 1 Basic Programs/CountDownIbuilt.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 from dateutil import relativedelta
-date1 = input("Enter first date (DD-MM-YYYY : \n")
-date2 = input("Enter second date (DD-MM-YYYY : \n")
-date_format = '%d-%m-%Y'
-date_object1 = datetime.strptime(date1, date_format)
-date_object2 = datetime.strptime(date2, date_format)
-diff_date = abs(relativedelta.relativedelta(date_object1,date_object2))
-print(f"{diff_date.days} days, {diff_date.months} months, {diff_date.years} years")
+try:
+    date1 = input("Enter first date (DD-MM-YYYY : \n")
+    date2 = input("Enter second date (DD-MM-YYYY : \n")
+    date_format = '%d-%m-%Y'
+    date_object1 = datetime.strptime(date1, date_format)
+    date_object2 = datetime.strptime(date2, date_format)
+    diff_date = abs(relativedelta.relativedelta(date_object1,date_object2))
+    print(f"{diff_date.days} days, {diff_date.months} months, {diff_date.years} years")
+except ValueError:
+    print("Enter numerical date in specified format")
